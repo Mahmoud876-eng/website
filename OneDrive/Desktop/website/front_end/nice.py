4690441c404039196aa12c8431f66ff8e59293bb
Mahmoud876-eng <mahmoud.malek@ismai.u-kairouan.tn>
2025-04-11 01:28:41 +0100

    now flask is better learned form is essential

--- OneDrive/Desktop/website/front_end/nice.py
+++ OneDrive/Desktop/website/front_end/nice.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
-@app.route("/", methods=["POST", "GET"] )
+
+@app.route("/")
+def home():
+    return render_template("home_page.html")
+
+@app.route("/login", methods=["POST","GET"] )
 def login():
     print(request.method)
     if request.method == "GET":
-        print("1")
+        print("2")
         return render_template("login.html")
+    print(request.method)
+    print("1")
     username = request.form.get("username")
+    print(username)
     if not username:
         return render_template("error.html", message="missing name")
     password = request.form.get("password")
@@ -15,24 +24,26 @@ def login():
     
     return render_template("login.html", name=username, password=password)
     
-@app.route("/register", methods=["POST", "GET"])
-def register():
-    
-    if request.method == "GET":
-        return render_template("register.html")
-    mail=request.args.get("mail")
+@app.route("/register", methods=["POST","GET"] )
+def index():
     print(request.method)
-    
+    if request.method == "GET":
+        print("3")
+        return render_template("register.html")
+    mail= request.form.get("mail") 
+    print(request.method)
+    print(mail)
     if not mail:
         print("5")
         return render_template("error.html", message="missing mail")
     if not mail.endswith("@gmail.com"):
         return render_template("error.html", message="missing @gmail.com")
     print("6")
-    username=request.args.get("username")
+    username=request.form.get("username")
+    print
     if not username:
         return render_template("error.html", message="missing name")
-    password=request.args.get("password")
+    password=request.form.get("password")
     if not password:
         return render_template("error.html", message="missing password")
     if len(password) < 8:
@@ -41,12 +52,9 @@ def register():
     return render_template("register.html", name=username, mail=mail, password=password) 
 @app.route("/insert")  
 def insert():
-    
-    if request.method == "POST":
-        print("1")
-        return render_template("insert.html")
-    if request.method == "GET":
-        print("2")
+    print(request.method)
+    if request.method == "git":
+        print("10")
         return render_template("insert.html")
     print(request.method)
     med_name=request.args.get("med_name")
@@ -56,17 +64,17 @@ def insert():
         return render_template("error.html", message="med_name should be letters only")
 
     return render_template("insert.html")
-@app.route("/interface", methods=["POST","GET"])
-def interface():
-    return render_template("interface.html")
 @app.route("/delet", methods=["POST","GET"])
 def delet():
     return render_template("delet.html")
+@app.route("/view", methods=["POST","GET"])
+def view():
+    return render_template("view.html")
 @app.route("/update", methods=["POST","GET"])
 def update():
     return render_template("update.html")
 if __name__ == '__main__':
-    debug=True # Enable debug mode
-    app.run()
+    #debug=True # Enable debug mode
+    app.run(debug=True)
 
 
